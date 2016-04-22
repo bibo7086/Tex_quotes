043c2a791a6b47397485af3e301b5f11b7974398
Saeed Ali <S@mac.local>
2016-04-22 09:26:22 +0300

    Initial Commit

---
+++ Tex_quotes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tex_quotes
+//
+//  Created by Saeed Ali on 22/04/2016.
+//  Copyright © 2016 Saeed Ali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
